27f53f51ff4d0b5f5bca3f45abbec05a7042ed14
Matteoguerra07 <matteo.guerra2233@gmail.com>
2026-01-29 21:33:50 +0100

    in parte sistemato il js

--- Vivaio/js/app.js
+++ Vivaio/js/app.js
@@ -1,4 +1,20 @@
-function load(){
-    const Alberi = document.getElementById("tabalberi")
-    JSON.parse
+function load() {
+    const sezione1 = document.getElementById("tabalberi1");
+    const sezione2 = document.getElementById("tabalberi2");
+
+    for (let i = 0; i < datiAlberi.alberi.length; i++) {
+        const html = 
+            '<li class="list-style">' +
+                '<img src="'+ datiAlberi.alberi[i].immagine +'" alt="" class="secimgsection">' +
+                '<div style="text-align: left;">' +
+                    '<h3 class="subtitle-plantsection">'+ datiAlberi.alberi[i].nome + '</h3>' +
+                    '<p>'+ datiAlberi.alberi[i].descrizione +'</p>' +
+                '</div>' +
+            '</li>';
+
+        sezione1.innerHTML += html;
+        sezione2.innerHTML += html;
+    }
 }
+
+load();
